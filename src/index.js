b2433477a4d19b78fc468d60eac33dbc8160405d
Karthik Raveendran <karthik.panicker@gmail.com>
2020-09-27 00:45:57 +0530

    Include qrious from npm.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './qrious.min';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'popper.js';
+import 'qrious/dist/qrious.min'
 import 'jquery/dist/jquery.slim';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
